616d841a3f5936d9f19ccf3b1fbd21cb8d53356a
jeffreywescott <jeffrey@learnersguild.org>
2016-04-18 11:03:42 -0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'learnersguild:rocketchat-lg-sso',
-  version: '0.2.3',
+  version: '0.2.4',
   summary: 'Accounts login handler for Learners Guild SSO.',
   git: 'https://github.com/LearnersGuild/rocketchat-lg-sso'
 })
