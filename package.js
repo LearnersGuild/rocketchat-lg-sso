ad5793fd758742e5bbe7bb72607964d6acbf6ddd
Sabra Pratt <pratt.sabra@gmail.com>
2016-06-03 10:50:41 -0700

    meteor package version bump?

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'learnersguild:rocketchat-lg-sso',
-  version: '0.4.7',
+  version: '0.4.8',
   summary: 'Accounts login handler for Learners Guild SSO.',
   git: 'https://github.com/LearnersGuild/rocketchat-lg-sso'
 })
